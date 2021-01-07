243566e356b3129df506e27960720f992da93c8a
Nathaniel Alvin <miao.cats.bot@gmail.com>
2021-01-08 02:01:12 +0700

    Added print board

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This is the sudoku puzzle
 board = [
     [0, 2, 0, 0, 3, 0, 0, 4, 0],
     [6, 0, 0, 0, 0, 0, 0, 0, 3],
@@ -7,4 +8,23 @@ board = [
     [0, 0, 0, 7, 0, 5, 0, 0, 0],
     [0, 0, 7, 0, 0, 0, 6, 0, 0],
     [4, 0, 0, 0, 0, 0, 0, 0, 8],
-    [0, 3, 0, 0, 4, 0, 0, 2, 0]]
+    [0, 3, 0, 0, 4, 0, 0, 2, 0]
+]
+
+
+def print_board(bo):
+    for row in range(len(bo)):  # iterating rows
+        if row % 3 == 0 and row != 0:
+            # print seperator between rows every 3 row
+            print("- - - - - - - - - -")
+        for column in range(len(bo[0])):  # range(number of digits in a row)
+            if column % 3 == 0 and column != 0:
+                print('|', end='')
+
+            if column == 8:  # on the last column, print without space at the end
+                print(bo[row][column])
+            else:
+                print(str(bo[row][column]) + ' ', end='')  # print the numbers
+
+
+print_board(board)
